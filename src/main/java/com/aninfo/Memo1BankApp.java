1668d7e6bf021b6324505c1bb5359df09c39e486
Martin Laurito <martinlaurito@hotmail.com>
2023-10-26 07:06:07 -0300

    agrege metodos API

--- src/main/java/com/aninfo/Memo1BankApp.java
+++ src/main/java/com/aninfo/Memo1BankApp.java
@@ -1,7 +1,14 @@
 package com.aninfo;
 
+import com.aninfo.exceptions.DepositNegativeSumException;
+import com.aninfo.exceptions.InexistantTransactionException;
+import com.aninfo.exceptions.InsufficientFundsException;
 import com.aninfo.model.Account;
+import com.aninfo.model.Deposit;
+import com.aninfo.model.Transaction;
+import com.aninfo.model.Withdraw;
 import com.aninfo.service.AccountService;
+import com.aninfo.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -26,28 +34,53 @@ public class Memo1BankApp {
 
 	@Autowired
 	private AccountService accountService;
+	@Autowired
+	private TransactionService transactionService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Memo1BankApp.class, args);
 	}
 
-	@PostMapping("/accounts")
+	@PostMapping("/accounts/{sum}")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Account createAccount(@RequestBody Account account) {
-		return accountService.createAccount(account);
+	public Account createAccount(@PathVariable Double sum) {
+		if (sum < 0) {
+			throw new DepositNegativeSumException("Account cannot be created with negative balance");
+		}
+		var account = new Account(sum);
+		var acc = accountService.createAccount(account);
+		var transaction = new Deposit(sum, account.getCbu());
+		var account1 = accountService.deposit(transaction, acc);
+		return account1;
 	}
-
 	@GetMapping("/accounts")
 	public Collection<Account> getAccounts() {
 		return accountService.getAccounts();
 	}
-
+	@GetMapping("/transactions")
+	public Collection<Transaction> getTransactions() {
+		return transactionService.getTransactions();
+	}
 	@GetMapping("/accounts/{cbu}")
 	public ResponseEntity<Account> getAccount(@PathVariable Long cbu) {
 		Optional<Account> accountOptional = accountService.findById(cbu);
 		return ResponseEntity.of(accountOptional);
 	}
-
+	@GetMapping("/transactions/{id}")
+	public ResponseEntity<Transaction> getTransaction(@PathVariable Long id) {
+		Optional<Transaction> transactionOptional = transactionService.findById(id);
+		return ResponseEntity.of(transactionOptional);
+	}
+	@GetMapping()
+	public ResponseEntity<List<Transaction>> getTransactionByAccount(@RequestParam Long cbu) {
+		Optional<Account> accountOptional = accountService.findById(cbu);
+		if (accountOptional.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		var acccount = accountOptional.get();
+		var transactions = acccount.getTransactions();
+		return ResponseEntity.ok(transactions);
+	}
 	@PutMapping("/accounts/{cbu}")
 	public ResponseEntity<Account> updateAccount(@RequestBody Account account, @PathVariable Long cbu) {
 		Optional<Account> accountOptional = accountService.findById(cbu);
@@ -64,15 +97,29 @@ public class Memo1BankApp {
 	public void deleteAccount(@PathVariable Long cbu) {
 		accountService.deleteById(cbu);
 	}
+	@DeleteMapping("/transactions/{id}")
+	public void deleteTransaction(@PathVariable Long id) {
+		Optional<Transaction> transactionOptional = transactionService.findById(id);
+		if (transactionOptional.isEmpty()) {
+			throw new InexistantTransactionException("Transaction doesn't exist");
+		}
+		var transaction = transactionOptional.get();
+		var acc = accountService.findById(transaction.getTransactionOwner());
+		var account = acc.get();
+		account.deleteTransaction(transaction.getID());
+		transactionService.deleteById(transaction.getID());
+	}
 
 	@PutMapping("/accounts/{cbu}/withdraw")
 	public Account withdraw(@PathVariable Long cbu, @RequestParam Double sum) {
-		return accountService.withdraw(cbu, sum);
+		var transaction = new Withdraw(-sum, cbu);
+		return accountService.withdraw(transaction);
 	}
 
 	@PutMapping("/accounts/{cbu}/deposit")
 	public Account deposit(@PathVariable Long cbu, @RequestParam Double sum) {
-		return accountService.deposit(cbu, sum);
+		var transaction = new Deposit(sum, cbu);
+		return accountService.deposit(transaction);
 	}
 
 	@Bean
