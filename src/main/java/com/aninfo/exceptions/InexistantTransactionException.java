24c1487893e2fb80efdaaf242467da2a71efc627
Martin Laurito <martinlaurito@hotmail.com>
2023-10-26 07:11:21 -0300

    agrege excepcion no hay transaccción

--- src/main/java/com/aninfo/exceptions/InexistantTransactionException.java
+++ src/main/java/com/aninfo/exceptions/InexistantTransactionException.java
@@ -1,4 +1,9 @@
 package com.aninfo.exceptions;
 
-public class InexistantTransactionException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class InexistantTransactionException extends RuntimeException{
+    public InexistantTransactionException(String message) {super(message);}
 }
