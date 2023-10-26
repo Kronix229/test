28c82f8bf6aa25bcc10f0e2e36dd2fd07a91ad20
Martin Laurito <martinlaurito@hotmail.com>
2023-10-26 07:12:24 -0300

    agrege transacciones a cuenta y calculo balance en base a transacciones

--- src/main/java/com/aninfo/model/Account.java
+++ src/main/java/com/aninfo/model/Account.java
@@ -1,6 +1,10 @@
 package com.aninfo.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Entity
 public class Account {
@@ -8,16 +12,14 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long cbu;
+    @OneToMany()
+    private Map<Long, Transaction> transactions;
 
-    private Double balance;
-
-    public Account(){
+    public Account() {
     }
-
-    public Account(Double balance) {
-        this.balance = balance;
+    public Account(Double sum) {
+        transactions = new HashMap<>();
     }
-
     public Long getCbu() {
         return cbu;
     }
@@ -27,11 +29,16 @@ public class Account {
     }
 
     public Double getBalance() {
+        var balance = transactions.values().stream().mapToDouble(Transaction::getCharge).sum();
         return balance;
     }
-
-    public void setBalance(Double balance) {
-        this.balance = balance;
+    public void addTransaction(Transaction transaction) {this.transactions.put(transaction.getID(),transaction);}
+    public void deleteTransaction(Long id) {transactions.remove(id);}
+    public List<Transaction> getTransactions() {
+        var transactionList = new ArrayList<Transaction>();
+        for (Transaction transaction : transactions.values()) {
+            transactionList.add(transaction);
+        };
+        return transactionList;
     }
-
 }
