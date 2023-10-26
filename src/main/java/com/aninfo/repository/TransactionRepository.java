bd635d681208226f4576262db9363eea447e0cb2
Martin Laurito <martinlaurito@hotmail.com>
2023-10-26 07:10:38 -0300

    agrege persistencia de transferencias

--- src/main/java/com/aninfo/repository/TransactionRepository.java
+++ src/main/java/com/aninfo/repository/TransactionRepository.java
@@ -1,4 +1,15 @@
 package com.aninfo.repository;
 
-public interface TransactionRepository {
+import com.aninfo.model.Account;
+import com.aninfo.model.Transaction;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource
+public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+    @Override
+    List<Transaction> findAll();
 }
--- src/main/java/com/aninfo/service/TransactionService.java
+++ src/main/java/com/aninfo/service/TransactionService.java
@@ -1,4 +1,26 @@
 package com.aninfo.service;
 
+import com.aninfo.model.Account;
+import com.aninfo.model.Transaction;
+import com.aninfo.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public Collection<Transaction> getTransactions() {
+        return transactionRepository.findAll();
+    }
+    public Optional<Transaction> findById(Long id) {
+        return transactionRepository.findById(id);
+    }
+    public void deleteById(Long id) {
+        transactionRepository.deleteById(id);
+    }
 }
