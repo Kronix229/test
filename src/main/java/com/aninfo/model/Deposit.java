c9db1d9a48b558f93f34b2079df5e2ee4759058c
Martin Laurito <martinlaurito@hotmail.com>
2023-10-30 04:07:02 -0300

    agrege logica para aplicar promo

--- src/main/java/com/aninfo/model/Deposit.java
+++ src/main/java/com/aninfo/model/Deposit.java
@@ -3,9 +3,19 @@ package com.aninfo.model;
 import javax.persistence.Entity;
 
 @Entity
-public class Deposit extends Transaction{
+public class Deposit extends Transaction {
     public Deposit() {}
     public Deposit(Double sum, Long cbu) {
-        super(sum, cbu);
+            super(sum,cbu);
+            applyPromo(sum);
+    }
+    private void applyPromo(Double sum) {
+        if (sum >= 2000) {
+            if (sum < 5000) {
+                charge += sum/10;
+            } else {
+                charge += 500;
+            }
+        }
     }
 }
