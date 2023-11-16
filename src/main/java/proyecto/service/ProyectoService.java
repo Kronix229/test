bbafff0e26ccd47c7fe78b5b127624a97f024ee2
Martin Laurito <martinlaurito@hotmail.com>
2023-11-15 21:27:34 -0300

    agrege capa de service para proyectos y tareas

--- src/main/java/proyecto/service/ProyectoService.java
+++ src/main/java/proyecto/service/ProyectoService.java
@@ -1,4 +1,33 @@
 package proyecto.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import proyecto.model.Proyecto;
+import proyecto.repository.ProyectoRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
 public class ProyectoService {
+    @Autowired
+    private ProyectoRepository proyectoRepository;
+    public Proyecto createProyecto(Proyecto proyecto) {
+        return proyectoRepository.save(proyecto);
+    }
+    public Collection<Proyecto> getProyectos() {
+        return proyectoRepository.findAll();
+    }
+
+    public Optional<Proyecto> findById(Long id) {
+        return proyectoRepository.findById(id);
+    }
+
+    public void save(Proyecto proyecto) {
+        proyectoRepository.save(proyecto);
+    }
+
+    public void deleteById(Long id) {
+        proyectoRepository.deleteById(id);
+    }
 }
--- src/main/java/proyecto/service/TareaService.java
+++ src/main/java/proyecto/service/TareaService.java
@@ -1,4 +1,36 @@
 package proyecto.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import proyecto.model.Tarea;
+import proyecto.model.Tarea;
+import proyecto.repository.TareaRepository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TareaService {
+    @Autowired
+    private TareaRepository tareaRepository;
+    public Tarea createTarea(Tarea tarea) {
+        return tareaRepository.save(tarea);
+    }
+    public Collection<Tarea> getTareas() {
+        return tareaRepository.findAll();
+    }
+    public List<Tarea> getTareasPorProyecto(Long id) { return tareaRepository.findByProyecto(id); }
+
+    public Optional<Tarea> findById(Long id) {
+        return tareaRepository.findById(id);
+    }
+
+    public void save(Tarea tarea) {
+        tareaRepository.save(tarea);
+    }
+
+    public void deleteById(Long id) {
+        tareaRepository.deleteById(id);
+    }
 }
